196fd7e17af975f8b90565aefac5715fe374bcd1
baldpixels <baldpixels@gmail.com>
2017-12-28 16:12:36 -0700

    housecleaning

--- js/jsLinks.php
+++
@@ -1,4 +0,0 @@
-<!--<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>-->
-<script src="js/jquery-3.2.1.min.js"></script>
-<!--scripts by CJR-->
-<script src="js/home.js"></script>
