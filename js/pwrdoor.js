80984a7e8b0d061c3364617bda2199c9f270d7c3
baldpixels <baldpixels@gmail.com>
2018-01-19 11:55:42 -0600

    fixed the damn event listeners (.on was the key)

--- js/pwrdoor.js
+++ js/pwrdoor.js
@@ -28,25 +28,24 @@ $(document).ready(function(){
   $("#logo").click(pwrClick);
     $("#logo").hover(function(){
       this.src = "images/logo_hover.png";
-    });
-    $("#logo").mouseleave(function(){
+    },function(){
       this.src = "images/logo.png";
     });
 
-  $("#left").click(leftClick);
-    $("#left").hover(function(){
-      this.src = "images/left_hover.png";
+  $("#dashboard").on('click', '#left', leftClick);
+    $("#dashboard").on('mouseover', '#left', function(){
+      $("#left").attr("src","images/left_hover.png");
     });
-    $("#left").mouseleave(function(){
-      this.src = "images/left.png";
+    $("#dashboard").on('mouseout', '#left', function(){
+      $("#left").attr("src","images/left.png");
     });
 
-  $("#right").click(rightClick);
-    $("#right").hover(function(){
-      this.src = "images/right_hover.png";
+  $("#dashboard").on('click', '#right', rightClick);
+    $("#dashboard").on('mouseover', '#right', function(){
+      $("#right").attr("src","images/right_hover.png");
     });
-    $("#right").mouseleave(function(){
-      this.src = "images/right.png";
+    $("#dashboard").on('mouseout', '#right', function(){
+      $("#right").attr("src","images/right.png");
     });
 
 //functions
@@ -60,8 +59,10 @@ $(document).ready(function(){
   }
 
   function pwrClick(){
-    $("#logo").animate({bottom: "125px", width: "250px", opacity: "0"}, 500, "swing");
-    introAnimation();
+    $("#logo").css("z-index", "-1")
+    $("#logo").animate({top: "112px", width: "225px"}, 750, "swing", function(){
+      introAnimation();
+    });
   }
 
   function introAnimation(){
@@ -69,20 +70,27 @@ $(document).ready(function(){
     $("#dashboard").append("<img id='dash' src='images/dash.png' alt='' />");
     $("#dashboard").append("<img id='vlad' src='images/vlad.png' alt='' />");
 
-    $("#dash").animate({top: "0px"}, 500, "swing");
-    $("#vlad").animate({bottom: "0px"}, 500, "swing");
+    $("#vlad").animate({bottom: "0px"}, 625, "swing");
+    $("#dash").animate({top: "0px"}, 1250, "easeOutBounce", function(){
+      $("#logo").fadeOut(100);
 
-    $("#dashboard").append("<img id='left' src='images/left.png' alt='' />");
-    $("#dashboard").append("<img id='right' src='images/right.png' alt='' />");
+      $("#dashboard").append("<img id='left' src='images/left.png' alt='' />");
+      $("#dashboard").append("<img id='right' src='images/right.png' alt='' />");
+
+      $("#debug").html("<p>intro complete</p>");
+    });
+
   }
 
   function leftClick(){
+    $("#debug").html("<p>leftClick</p>")
     $("#dashboard").animate({left: "-33%"}, 500, "swing");
     $("#content").animate({right: "0px"}, 500, "swing");
   }
 
   function rightClick(){
-    $("#dashboard").animate({right: "-33%"}, 500, "swing");
+    $("#debug").html("<p>rightClick</p>")
+    $("#dashboard").animate({left: "+33%"}, 500, "swing");
     $("#content").animate({left: "0px"}, 500, "swing");
   }
 
