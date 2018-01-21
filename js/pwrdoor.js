72641666c5349145c3f714ab9a6e2f628268564e
baldpixels <baldpixels@gmail.com>
2018-01-20 18:43:27 -0600

    updated icons; overflow works

--- js/pwrdoor.js
+++ js/pwrdoor.js
@@ -13,7 +13,6 @@ $(document).ready(function(){
     var portWidth;
 
 //on load
-  $("#BG").hide();
   updatePort();
 
 //event listeners
@@ -32,21 +31,53 @@ $(document).ready(function(){
       this.src = "images/logo.png";
     });
 
-  $("#dashboard").on('click', '#left', leftClick);
+  $("#dashboard").on('click', '#left_hover', leftClick);
     $("#dashboard").on('mouseover', '#left', function(){
-      $("#left").attr("src","images/left_hover.png");
+      $("#left_hover").show();
     });
-    $("#dashboard").on('mouseout', '#left', function(){
-      $("#left").attr("src","images/left.png");
+    $("#dashboard").on('mouseout', '#left_hover', function(){
+      $("#left_hover").hide();
     });
 
-  $("#dashboard").on('click', '#right', rightClick);
+  $("#dashboard").on('click', '#right_hover', rightClick);
     $("#dashboard").on('mouseover', '#right', function(){
-      $("#right").attr("src","images/right_hover.png");
+      $("#right_hover").show();
     });
-    $("#dashboard").on('mouseout', '#right', function(){
-      $("#right").attr("src","images/right.png");
+    $("#dashboard").on('mouseout', '#right_hover', function(){
+      $("#right_hover").hide();
     });
+
+  $("#dashboard").on('click', '#brights_hover', brightsClick);
+    $("#dashboard").on('mouseover', '#brights', function(){
+      $("#brights_hover").show();
+    });
+    $("#dashboard").on('mouseout', '#brights_hover', function(){
+      $("#brights_hover").hide();
+    });
+
+    $("#dashboard").on('click', '#M_hover', MClick);
+      $("#dashboard").on('mouseover', '#M', function(){
+        $("#M_hover").show();
+      });
+      $("#dashboard").on('mouseout', '#M_hover', function(){
+        $("#M_hover").hide();
+      });
+
+    $("#dashboard").on('click', '#G_hover', GClick);
+      $("#dashboard").on('mouseover', '#G', function(){
+        $("#G_hover").show();
+      });
+      $("#dashboard").on('mouseout', '#G_hover', function(){
+        $("#G_hover").hide();
+      });
+
+    $("#dashboard").on('click', '#P_hover', PClick);
+      $("#dashboard").on('mouseover', '#P', function(){
+        $("#P_hover").show();
+      });
+      $("#dashboard").on('mouseout', '#P_hover', function(){
+        $("#P_hover").hide();
+      });
 
 //functions
   function updateDebug(){
@@ -75,7 +106,30 @@ $(document).ready(function(){
       $("#logo").fadeOut(100);
 
       $("#dashboard").append("<img id='left' src='images/left.png' alt='' />");
+      $("#dashboard").append("<img id='left_hover' src='images/left_hover.png' alt='' />");
+
       $("#dashboard").append("<img id='right' src='images/right.png' alt='' />");
+      $("#dashboard").append("<img id='right_hover' src='images/right_hover.png' alt='' />");
+
+      $("#dashboard").append("<img id='brights' src='images/brights.png' alt='' />");
+      $("#dashboard").append("<img id='brights_hover' src='images/brights_hover.png' alt='' />");
+
+      $("#dashboard").append("<img id='M' src='images/M.png' alt='' />");
+      $("#dashboard").append("<img id='M_hover' src='images/M_hover.png' alt='' />");
+
+      $("#dashboard").append("<img id='G' src='images/G.png' alt='' />");
+      $("#dashboard").append("<img id='G_hover' src='images/G_hover.png' alt='' />");
+
+      $("#dashboard").append("<img id='P' src='images/P.png' alt='' />");
+      $("#dashboard").append("<img id='P_hover' src='images/P_hover.png' alt='' />");
+
+
+      $("#left_hover").hide();
+      $("#right_hover").hide();
+      $("#brights_hover").hide();
+      $("#M_hover").hide();
+      $("#G_hover").hide();
+      $("#P_hover").hide();
 
       $("#debug").html("<p>intro complete</p>");
     });
@@ -85,13 +139,34 @@ $(document).ready(function(){
   function leftClick(){
     $("#debug").html("<p>leftClick</p>")
     $("#dashboard").animate({left: "-33%"}, 500, "swing");
-    $("#content").animate({right: "0px"}, 500, "swing");
+    $("#content").animate({left: "66%"}, 500, "swing");
   }
 
   function rightClick(){
     $("#debug").html("<p>rightClick</p>")
     $("#dashboard").animate({left: "+33%"}, 500, "swing");
-    $("#content").animate({left: "0px"}, 500, "swing");
+    $("#content").animate({left: "33%"}, 500, "swing");
+  }
+
+  function brightsClick(){
+    $("#debug").html("<p>brightsClick</p>")
+    $("#dashboard").animate({left: "0px"}, 500, "swing");
+    $("#content").animate({left: "50%"}, 500, "swing");
+  }
+
+  function MClick(){
+    $("#debug").html("<p>Movies</p>")
+    for(var i=0; i<25; i++){
+      $("#content").append("<p>Movie "+i+"</p>");
+    }
+  }
+
+  function GClick(){
+    $("#debug").html("<p>Graphics</p>")
+  }
+
+  function PClick(){
+    $("#debug").html("<p>Photos</p>")
   }
 
 
